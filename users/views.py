41f6803b18cbf217c01f11167521eb2fcdbe14b9
NikitaTutik <famounst@gmail.com>
2023-08-27 21:43:59 +0300

    permissions for user requests

--- users/views.py
+++ users/views.py
@@ -2,11 +2,19 @@ from rest_framework import generics
 from .models import CustomUser
 from .serializers import CustomTokenObtainPairSerializer, UserSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        method = self.request.method
+        if method == "POST":
+            return [AllowAny()]
+        else:
+            return [IsAuthenticated()]
 
 
 class UserRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):

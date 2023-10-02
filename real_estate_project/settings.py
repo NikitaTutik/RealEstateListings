ff0653704ca8bc124ee18017e5f43e57ee3aff2c
NikitaTutik <famounst@gmail.com>
2023-10-02 22:18:56 +0300

    debug false

--- real_estate_project/settings.py
+++ real_estate_project/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = ["real-estate-listingsapi.onrender.com", "127.0.0.1", "localhost"]
 

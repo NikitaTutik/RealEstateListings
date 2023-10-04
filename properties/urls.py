e6d1328d9814fc5de00910068d29494730bbea30
NikitaTutik <famounst@gmail.com>
2023-10-04 22:27:33 +0300

    urlpatters on properties

--- properties/urls.py
+++ properties/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import PropertyListCreateView, PropertyRetrieveUpdateDeleteView
 
 urlpatterns = [
     path('properties/', PropertyListCreateView.as_view(), name='property-list-create'),
     path('properties/<int:pk>/', PropertyRetrieveUpdateDeleteView.as_view(), name='property-detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

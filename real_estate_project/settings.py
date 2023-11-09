3658900538e98ba44b179be784d99d86f0fd5708
NikitaTutik <famounst@gmail.com>
2023-11-09 18:40:28 +0300

    pagination

--- real_estate_project/settings.py
+++ real_estate_project/settings.py
@@ -172,6 +172,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
 }
 
 SWAGGER_SETTINGS = {
